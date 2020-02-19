fbdf45512bcf5f94ac42e1f76a692c4388fff9af
maklet <makletlindfors@gmail.com>
2020-02-19 11:20:58 +0100

    hallå igen GITHUB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // härifrån startar vi upp vår webshop
+const mongoose = require('mongoose')
 const { app, port } = require('./src/server')
+const config = require('./config/config')
 
-const server = app.listen(port, () => console.log('Live on port 8080'))
-
-module.exports = server
+// kicka igång servern
+const options = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(config.databaseURL, options).then(() => {
+    app.listen(port, () => console.log('Live on port 8080'))
+})
